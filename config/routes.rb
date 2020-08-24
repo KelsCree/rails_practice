a4916789ab67d6f106248b8515aff874c8ff541f
KelsCree <kelseycreehan@gmail.com>
2020-08-24 17:59:57 -0600

    Make build and burn

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "cars" , to: "cars#index"
+  # get "cars/:id" , to: "cars#show"
+  # delete "cars/:id", to: "cars#destroy"
+  # update "cars/:id", to: "cars#update"
+  # post "cars", to: "cars#create"
+
+  resources :cars
+end
